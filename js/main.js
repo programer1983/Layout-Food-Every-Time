526c1be2c4dbc268f2c47449cc25b92fc96325c2
unknown <programer972@gmail.com>
2024-08-06 15:19:06 +0300

    adding interactive effects

--- js/main.js
+++ js/main.js
@@ -6,3 +6,393 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.reviews__arrow--prev',
     },
 });
+
+/* =========================================================================== */
+
+const animHeroTitles = document.querySelectorAll('.hero__title')
+
+if (animHeroTitles.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animHeroTitles.length; index++){
+            const animItem = animHeroTitles[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* =================================================================================== */
+
+const animHeeroDisc = document.querySelectorAll('.hero__description')
+
+if (animHeeroDisc.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animHeeroDisc.length; index++){
+            const animItem = animHeeroDisc[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* =================================================================================== */
+
+const animForm = document.querySelectorAll('.hero__form')
+
+if (animForm.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animForm.length; index++){
+            const animItem = animForm[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* =================================================================================== */
+
+const animPopular = document.querySelectorAll('.popular__title')
+
+if (animPopular.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animPopular.length; index++){
+            const animItem = animPopular[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* =================================================================================== */
+
+const animDescription = document.querySelectorAll('.popular__description')
+
+if (animDescription.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animDescription.length; index++){
+            const animItem = animDescription[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* =================================================================================== */
+
+const animItem1 = document.querySelectorAll('.popular--item-1')
+
+if (animItem1.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animItem1.length; index++){
+            const animItem = animItem1[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* ==================================================================================== */
+
+const animItem2 = document.querySelectorAll('.popular--item-2')
+
+if (animItem2.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animItem2.length; index++){
+            const animItem = animItem2[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* ==================================================================================== */
+
+const animItem3 = document.querySelectorAll('.popular--item-3')
+
+if (animItem3.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animItem3.length; index++){
+            const animItem = animItem3[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* ==================================================================================== */
+
+const animContent = document.querySelectorAll('.about__content')
+
+if (animContent.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animContent.length; index++){
+            const animItem = animContent[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
+
+/* ==================================================================================== */
+
+const animImages = document.querySelectorAll('.about__images')
+
+if (animImages.length > 0){
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(){
+        for (let index = 0; index < animImages.length; index++){
+            const animItem = animImages[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffset = offset(animItem).top
+            const animStart = 4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+
+            if (animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight  / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint ) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active')
+            }else{
+                if (!animItem.classList.contains('anim-no-haight')){
+                    animItem.classList.remove('active')
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
